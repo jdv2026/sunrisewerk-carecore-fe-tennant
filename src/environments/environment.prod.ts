8ef87a0e24f0d3b7ad3cc7903f1d097d2ed766ca
jhon <jhondennivistal@gmail.com>
2026-04-27 12:12:49 +0800

    Tennants registration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,12 @@
 export const environment = {
     production: true,
     cognito: {
-        userPoolId: 'us-east-1_XXXXXXXXX',
-        userPoolClientId: 'XXXXXXXXXXXXXXXXXXXXXXXXXX',
-        region: 'us-east-1',
+        userPoolId: 'PROD_USER_POOL_ID',
+        userPoolClientId: 'PROD_USER_POOL_CLIENT_ID',
+        region: 'ap-southeast-1',
+        oauthDomain: 'PROD_OAUTH_DOMAIN',
+        redirectSignIn: 'https://PROD_DOMAIN/auth/login',
+        redirectSignOut: 'https://PROD_DOMAIN/auth/login',
     },
-    backendApi: 'https://dev.sunrisewerk.com/',
+    backendApi: 'https://PROD_BACKEND_API/',
 };
